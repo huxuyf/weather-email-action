4c7f3bd32d4de2f4f5dbc4429e5dffd2789d4dac
huxuyf <shingu@gmail.com>
2023-03-23 11:34:11 +0800

    html改成图片

--- weather.sh
+++ weather.sh
@@ -6,4 +6,5 @@ LANGUAGE="zh-CN"
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
 CITY=Wuxing
 
-curl -H "Accept-Language: $LANGUAGE" -H "User-Agent: $UA" -o result.html https://wttr.in/$CITY
+# curl -H "Accept-Language: $LANGUAGE" -H "User-Agent: $UA" -o result.html https://wttr.in/$CITY
+curl -H "Accept-Language:$LANGUAGE" -H "User-Agent:$UA" https://wttr.in/$CITY.png > $CITY.png
